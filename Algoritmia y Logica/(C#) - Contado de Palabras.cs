b555a154b2cc86ec2df77349c91bf76548e748d8
LautaroPavanSarquis <98368392+LautaroPavanSarquis@users.noreply.github.com>
2025-01-29 00:38:39 -0300

    Add files via upload

---
+++ Algoritmia y Logica/(C#) - Contado de Palabras.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void wordCount(string texto)
+    {
+        Dictionary<string, int> palabraContada = new Dictionary<string, int>();
+
+        // Limpiar el string
+        string[] palabras = texto.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        //Recorrer 
+        foreach (string palabra in palabras)
+        {
+            if (palabraContada.ContainsKey(palabra))
+            {
+                palabraContada[palabra]++;
+            }
+            else
+            {
+                  palabraContada.Add(palabra, 1);
+            }
+        }
+
+        // Mostrar
+        foreach (var entry in palabraContada)
+        {
+            Console.WriteLine($"Palabra: {entry.Key}, Contador: {entry.Value}");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string text = "The Rolling Stones are a rock band from London. " +
+            "Formed in 1962, they created hits like 'Satisfaction'. " +
+            "Mick Jagger and Keith Richards defined rock music and influenced generations.";
+
+        wordCount(text);
+    }
+}
+
