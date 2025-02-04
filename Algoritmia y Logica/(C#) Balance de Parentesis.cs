01bb8fbbb59d98f41ac6f5089612ea9c3bdce1db
LautaroPavanSarquis <98368392+LautaroPavanSarquis@users.noreply.github.com>
2025-02-04 15:12:05 -0300

    Add files via upload

---
+++ Algoritmia y Logica/(C#) Balance de Parentesis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void balanceDeParéntesis(string texto)
+        {
+            int count = 0; //cuenta "(" ")"
+
+            foreach (char caracter in texto)
+            {
+                switch (caracter)
+                {
+                    case '(':
+                        count++;
+                        break;
+
+                    case ')':
+                        count--;
+
+                        if (count < 0)
+                        {
+                            Console.WriteLine("Los parentesis no estan balanceados"); 
+                            return;
+                        }
+                        
+                        break;
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("Los parentesis estan balanceados");
+            }
+            else
+            {
+                Console.WriteLine("Los parentesis no estan balanceados");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            balanceDeParéntesis("            ");
+            balanceDeParéntesis(")(");
+            balanceDeParéntesis("(Este es un (ejemplo) de texto con (paréntesis) balanceados).");
+            balanceDeParéntesis("(Este (texto no) está balanceado porque falta un cierre (.            ");
+        }
+    }
+}
